9271cfe4ab91bc182a8115cff9feaf1446c17655
My Nguyen <myoutybay@gmail.com>
2021-11-24 17:05:30 +0700

    add theme

---
+++ src/components/_dashboard/department/index.js
@@ -0,0 +1,3 @@
+export { default as DepartListHead } from './DepartListHead';
+export { default as DepartListToolbar } from './DepartListToolbar';
+export { default as DepartMoreMenu } from './DepartMoreMenu';
---
+++ src/routes.js
@@ -0,0 +1,53 @@
+import { Navigate, useRoutes } from 'react-router-dom';
+// layouts
+import DashboardLayout from './layouts/dashboard';
+import LogoOnlyLayout from './layouts/LogoOnlyLayout';
+//
+import Login from './pages/Login';
+import Register from './pages/Register';
+import DashboardApp from './pages/DashboardApp';
+import Products from './pages/Products';
+import Blog from './pages/Blog';
+import User from './pages/User';
+import NotFound from './pages/Page404';
+import Profile from './pages/organization/Profile';
+import Department from './pages/organization/Department';
+
+// ----------------------------------------------------------------------
+
+export default function Router() {
+  return useRoutes([
+    {
+      path: '/dashboard',
+      element: <DashboardLayout />,
+      children: [
+        { element: <Navigate to="/dashboard/app" replace /> },
+        { path: 'app', element: <DashboardApp /> },
+        { path: 'user', element: <User /> },
+        { path: 'profile', element: <Profile /> },
+        { path: 'products', element: <Products /> },
+        { path: 'blog', element: <Blog /> }
+      ]
+    },
+    {
+      path: '/organization',
+      element: <DashboardLayout />,
+      children: [
+        { path: 'profile', element: <Profile /> },
+        { path: 'department', element: <Department /> }
+      ]
+    },
+    {
+      path: '/',
+      element: <LogoOnlyLayout />,
+      children: [
+        { path: 'login', element: <Login /> },
+        { path: 'register', element: <Register /> },
+        { path: '404', element: <NotFound /> },
+        { path: '/', element: <Navigate to="/dashboard" /> },
+        { path: '*', element: <Navigate to="/404" /> }
+      ]
+    },
+    { path: '*', element: <Navigate to="/404" replace /> }
+  ]);
+}
