1aabbd72dff325830b10d05b8e520ce3b1e3b1b1
enditidev <enditidev@gmail.com>
2021-12-10 18:00:49 +0700

    Updated

---
+++ src/components/_dashboard/overtimeshift/OvertimeShiftMoreMenu.js
@@ -0,0 +1,199 @@
+/* eslint-disable no-restricted-globals */
+import { Icon } from '@iconify/react';
+import { useRef, useState } from 'react';
+import { useFormik, Form, FormikProvider } from 'formik';
+import editFill from '@iconify/icons-eva/edit-fill';
+import { Link as RouterLink } from 'react-router-dom';
+import trash2Outline from '@iconify/icons-eva/trash-2-outline';
+import moreVerticalFill from '@iconify/icons-eva/more-vertical-fill';
+// material
+import {
+  Menu,
+  MenuItem,
+  IconButton,
+  ListItemIcon,
+  ListItemText,
+  Modal,
+  Box,
+  Stack,
+  Typography,
+  TextField
+} from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import TimePicker from '@mui/lab/TimePicker';
+import axios from '../../../functions/Axios';
+// ----------------------------------------------------------------------
+
+export default function OvertimeShiftMoreMenu(Shift) {
+  const ref = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const [timeStart, setTimeStart] = useState([]);
+  const [timeEnd, setTimeEnd] = useState([]);
+  const [open, setOpen] = useState(false);
+  const handleClose = () => setOpen(false);
+
+  const convertDateTime = (date) => {
+    const newDate = new Date(date);
+    const hour = newDate.getHours();
+    const min = newDate.getMinutes();
+    const sec = newDate.getSeconds();
+    return `${hour}:${min}:${sec}`;
+  };
+  const style = {
+    position: 'relative',
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    p: 4
+  };
+  const formik = useFormik({
+    initialValues: {
+      ShiftID: '',
+      ShiftName: '',
+      StartShift: convertDateTime(timeStart),
+      EndShift: convertDateTime(timeEnd)
+    },
+    onSubmit: () => {
+      axios
+        .post(`Organization/AddOrEditShift`, {
+          ShiftID: formik.values.ShiftID,
+          ShiftName: formik.values.ShiftName,
+          StartShift: convertDateTime(timeStart),
+          EndShift: convertDateTime(timeEnd)
+        })
+        .then((res) => {
+          if (res.data.Status === 'Updated') {
+            alert('Shift Updated');
+            window.location.reload();
+          } else {
+            alert('Shift not Updated');
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  });
+  const handleOpen = () => {
+    formik.setFieldValue('ShiftID', Shift.dulieu.ShiftID);
+    formik.setFieldValue('ShiftName', Shift.dulieu.ShiftName);
+    formik.setFieldValue('StartShift', Shift.dulieu.StartShift);
+    formik.setFieldValue('EndShift', Shift.dulieu.EndShift);
+    setTimeStart(new Date(`12/12/2000 ${Shift.dulieu.StartShift}`));
+    setTimeEnd(new Date(`12/12/2000 ${Shift.dulieu.EndShift}`));
+    setOpen(true);
+  };
+  const { handleSubmit, getFieldProps } = formik;
+  return (
+    <>
+      <IconButton ref={ref} onClick={() => setIsOpen(true)}>
+        <Icon icon={moreVerticalFill} width={20} height={20} />
+      </IconButton>
+
+      <Menu
+        open={isOpen}
+        anchorEl={ref.current}
+        onClose={() => setIsOpen(false)}
+        PaperProps={{
+          sx: { width: 200, maxWidth: '100%' }
+        }}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      >
+        <MenuItem
+          onClick={() => {
+            if (confirm('Are you sure you want to delete this shift?')) {
+              axios.delete(`Organization/DeleteShift?ID=${Shift.dulieu.ShiftID}`).then((res) => {
+                if (res.data.Status === 'Delete') {
+                  alert('Shift Deleted');
+                  window.location.reload();
+                } else {
+                  alert('Shift Not Deleted');
+                }
+              });
+            }
+          }}
+          sx={{ color: 'text.secondary' }}
+        >
+          <ListItemIcon>
+            <Icon icon={trash2Outline} width={24} height={24} />
+          </ListItemIcon>
+          <ListItemText primary="Delete" primaryTypographyProps={{ variant: 'body2' }} />
+        </MenuItem>
+
+        <MenuItem
+          onClick={handleOpen}
+          component={RouterLink}
+          to="#"
+          sx={{ color: 'text.secondary' }}
+        >
+          <ListItemIcon>
+            <Icon icon={editFill} width={24} height={24} />
+          </ListItemIcon>
+          <ListItemText primary="Edit" primaryTypographyProps={{ variant: 'body2' }} />
+        </MenuItem>
+        <Modal
+          open={open}
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <FormikProvider value={formik}>
+            <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
+              <Box sx={style}>
+                <Stack spacing={1}>
+                  <Typography id="modal-modal-title" variant="h6" component="h2">
+                    Edit Shift
+                  </Typography>
+                  <Stack direction={{ xs: 'column', sm: 'row' }} spacing={2}>
+                    <TextField
+                      fullWidth
+                      label="Shift Name"
+                      {...getFieldProps('ShiftName')}
+                      variant="outlined"
+                    />
+                  </Stack>
+                  <Stack direction={{ xs: 'column', sm: 'row' }} spacing={2}>
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                      <TimePicker
+                        label="Time Start"
+                        views={['hours', 'minutes', 'seconds']}
+                        value={timeStart}
+                        inputFormat="HH:mm:ss"
+                        onChange={(newValue) => {
+                          setTimeStart(newValue);
+                        }}
+                        renderInput={(params) => <TextField {...params} />}
+                      />
+                    </LocalizationProvider>
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                      <TimePicker
+                        label="Time End"
+                        value={timeEnd}
+                        views={['hours', 'minutes', 'seconds']}
+                        inputFormat="HH:mm:ss"
+                        onChange={(newValue) => {
+                          setTimeEnd(newValue);
+                        }}
+                        renderInput={(params) => <TextField {...params} />}
+                      />
+                    </LocalizationProvider>
+                  </Stack>
+                  <LoadingButton fullWidth size="large" type="submit" variant="contained">
+                    Edit Shift
+                  </LoadingButton>
+                </Stack>
+              </Box>
+            </Form>
+          </FormikProvider>
+        </Modal>
+      </Menu>
+    </>
+  );
+}
---
+++ src/pages/organization/OverTimeShift.js
@@ -0,0 +1,360 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import { filter } from 'lodash';
+import { Icon } from '@iconify/react';
+import { useState, useEffect } from 'react';
+import { useFormik, Form, FormikProvider } from 'formik';
+import plusFill from '@iconify/icons-eva/plus-fill';
+import { Link as RouterLink } from 'react-router-dom';
+// material
+import {
+  Card,
+  Table,
+  Stack,
+  Button,
+  Checkbox,
+  TableRow,
+  TableBody,
+  TableCell,
+  Container,
+  Typography,
+  TableContainer,
+  TablePagination,
+  Modal,
+  TextField,
+  Box
+} from '@mui/material';
+
+import { LoadingButton } from '@mui/lab';
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import TimePicker from '@mui/lab/TimePicker';
+import axios from '../../functions/Axios';
+
+// components
+import Page from '../../components/Page';
+import Scrollbar from '../../components/Scrollbar';
+import SearchNotFound from '../../components/SearchNotFound';
+import {
+  OvertimeShiftListHead,
+  OvertimeShiftListToolbar,
+  OvertimeShiftMoreMenu
+} from '../../components/_dashboard/overtimeshift';
+//
+
+import { getAllShift } from '../../functions/Organization';
+
+// ----------------------------------------------------------------------
+
+const TABLE_HEAD = [
+  { id: 'ShiftID', label: 'ShiftID', alignRight: false },
+  { id: 'ShiftName', label: 'Shift Name', alignRight: false },
+  { id: 'StartShift', label: 'Start Shift', alignRight: false },
+  { id: 'EndShift', label: 'End Shift', alignRight: false },
+  { id: '' }
+];
+
+// ----------------------------------------------------------------------
+
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => descendingComparator(a, b, orderBy)
+    : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+function applySortFilter(array, comparator, query) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) return order;
+    return a[1] - b[1];
+  });
+  if (query) {
+    return filter(array, (_user) => _user.name.toLowerCase().indexOf(query.toLowerCase()) !== -1);
+  }
+  return stabilizedThis.map((el) => el[0]);
+}
+
+export default function Shift() {
+  const [page, setPage] = useState(0);
+  const [order, setOrder] = useState('asc');
+  const [selected, setSelected] = useState([]);
+  const [shift, setShift] = useState([]);
+  const [orderBy, setOrderBy] = useState('name');
+  const [filterName, setFilterName] = useState('');
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [timeStart, setTimeStart] = useState(null);
+  const [timeEnd, setTimeEnd] = useState(null);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  useEffect(() => {
+    getAllShift().then((res) => {
+      setShift(res);
+    });
+  }, []);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelecteds = shift.map((n) => n.name);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
+    }
+    setSelected(newSelected);
+  };
+
+  const convertDateTime = (date) => {
+    const newDate = new Date(date);
+    const hour = newDate.getHours();
+    const min = newDate.getMinutes();
+    const sec = newDate.getSeconds();
+    return `${hour}:${min}:${sec}`;
+  };
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleFilterByName = (event) => {
+    setFilterName(event.target.value);
+  };
+  const style = {
+    position: 'relative',
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    p: 4
+  };
+  const formik = useFormik({
+    initialValues: {
+      ShiftName: '',
+      StartShift: convertDateTime(timeStart),
+      EndShift: convertDateTime(timeEnd)
+    },
+    onSubmit: () => {
+      axios
+        .post(`Organization/AddOrEditShift`, {
+          ShiftName: formik.values.ShiftName,
+          StartShift: convertDateTime(timeStart),
+          EndShift: convertDateTime(timeEnd)
+        })
+        .then((res) => {
+          if (res.data.Status === 'Success') {
+            alert('Thêm thành công');
+            window.location.reload();
+          } else {
+            alert('Thêm thất bại');
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  });
+  const { handleSubmit, getFieldProps } = formik;
+
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - shift.length) : 0;
+
+  const filteredUsers = applySortFilter(shift, getComparator(order, orderBy), filterName);
+
+  const isUserNotFound = filteredUsers.length === 0;
+
+  return (
+    <Page title="Shift | ChamCongVN">
+      <Modal
+        open={open}
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <FormikProvider value={formik}>
+          <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
+            <Box sx={style}>
+              <Stack spacing={1}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                  Add Shift
+                </Typography>
+                <Stack direction={{ xs: 'column', sm: 'row' }} spacing={2}>
+                  <TextField
+                    fullWidth
+                    label="Shift Name"
+                    {...getFieldProps('ShiftName')}
+                    variant="outlined"
+                  />
+                </Stack>
+                <Stack direction={{ xs: 'column', sm: 'row' }} spacing={2}>
+                  <LocalizationProvider dateAdapter={AdapterDateFns}>
+                    <TimePicker
+                      label="Time Start"
+                      views={['hours', 'minutes', 'seconds']}
+                      inputFormat="HH:mm:ss"
+                      value={timeStart}
+                      onChange={(newValue) => {
+                        setTimeStart(newValue);
+                      }}
+                      renderInput={(params) => <TextField {...params} />}
+                    />
+                  </LocalizationProvider>
+                  <LocalizationProvider dateAdapter={AdapterDateFns}>
+                    <TimePicker
+                      label="Time End"
+                      value={timeEnd}
+                      views={['hours', 'minutes', 'seconds']}
+                      inputFormat="HH:mm:ss"
+                      onChange={(newValue) => {
+                        setTimeEnd(newValue);
+                      }}
+                      renderInput={(params) => <TextField {...params} />}
+                    />
+                  </LocalizationProvider>
+                </Stack>
+                <LoadingButton fullWidth size="large" type="submit" variant="contained">
+                  Add Shift
+                </LoadingButton>
+              </Stack>
+            </Box>
+          </Form>
+        </FormikProvider>
+      </Modal>
+      <Container>
+        <Stack direction="row" alignItems="center" justifyContent="space-between" mb={5}>
+          <Typography variant="h4" gutterBottom>
+            Shift
+          </Typography>
+          <Button
+            onClick={handleOpen}
+            variant="contained"
+            component={RouterLink}
+            to="#"
+            startIcon={<Icon icon={plusFill} />}
+          >
+            New Shift
+          </Button>
+        </Stack>
+
+        <Card>
+          <OvertimeShiftListToolbar
+            numSelected={selected.length}
+            filterName={filterName}
+            onFilterName={handleFilterByName}
+          />
+
+          <Scrollbar>
+            <TableContainer sx={{ minWidth: 800 }}>
+              <Table>
+                <OvertimeShiftListHead
+                  order={order}
+                  orderBy={orderBy}
+                  headLabel={TABLE_HEAD}
+                  rowCount={shift.length}
+                  numSelected={selected.length}
+                  onRequestSort={handleRequestSort}
+                  onSelectAllClick={handleSelectAllClick}
+                />
+                <TableBody>
+                  {filteredUsers
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((row) => {
+                      const { ShiftID, ShiftName, StartShift, EndShift } = row;
+                      const isItemSelected = selected.indexOf(ShiftName) !== -1;
+
+                      return (
+                        <TableRow
+                          hover
+                          key={ShiftID}
+                          tabIndex={-1}
+                          role="checkbox"
+                          selected={isItemSelected}
+                          aria-checked={isItemSelected}
+                        >
+                          <TableCell padding="checkbox">
+                            <Checkbox
+                              checked={isItemSelected}
+                              onChange={(event) => handleClick(event, ShiftName)}
+                            />
+                          </TableCell>
+                          <TableCell align="left">{ShiftID}</TableCell>
+                          <TableCell align="left">{ShiftName}</TableCell>
+                          <TableCell align="left">{StartShift}</TableCell>
+                          <TableCell align="left">{EndShift}</TableCell>
+                          <TableCell align="right">
+                            <OvertimeShiftMoreMenu dulieu={row} />
+                          </TableCell>
+                        </TableRow>
+                      );
+                    })}
+                  {emptyRows > 0 && (
+                    <TableRow style={{ height: 53 * emptyRows }}>
+                      <TableCell colSpan={6} />
+                    </TableRow>
+                  )}
+                </TableBody>
+                {isUserNotFound && (
+                  <TableBody>
+                    <TableRow>
+                      <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
+                        <SearchNotFound searchQuery={filterName} />
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                )}
+              </Table>
+            </TableContainer>
+          </Scrollbar>
+
+          <TablePagination
+            rowsPerPageOptions={[5, 10, 25]}
+            component="div"
+            count={shift.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onPageChange={handleChangePage}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Card>
+      </Container>
+    </Page>
+  );
+}
