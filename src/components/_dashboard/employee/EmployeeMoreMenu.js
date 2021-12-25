b678a92586ca8683821f95659688f02185cc5d7a
My Nguyen <myoutybay@gmail.com>
2021-12-25 14:10:43 +0700

    Specialities

--- src/components/_dashboard/employee/EmployeeMoreMenu.js
+++ src/components/_dashboard/employee/EmployeeMoreMenu.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-restricted-globals */
 import { Icon } from '@iconify/react';
-import { useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { useFormik, Form, FormikProvider } from 'formik';
 import editFill from '@iconify/icons-eva/edit-fill';
 import { Link as RouterLink } from 'react-router-dom';
@@ -17,18 +17,68 @@ import {
   Box,
   Stack,
   Typography,
-  TextField
+  FormControl,
+  InputLabel,
+  Select,
+  OutlinedInput,
+  Checkbox
 } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
 import axios from '../../../functions/Axios';
 
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250
+    }
+  }
+};
+const names = [
+  'Oliver Hansen',
+  'Van Henry',
+  'April Tucker',
+  'Ralph Hubbard',
+  'Omar Alexander',
+  'Carlos Abbott',
+  'Miriam Wagner',
+  'Bradley Wilkerson',
+  'Virginia Andrews',
+  'Kelly Snyder'
+];
 export default function EmployeeMoreMenu({ dulieu, handleOpenToast }) {
   const { EmployeeID } = dulieu.emp;
   const ref = useRef(null);
   const [loading, setLoading] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+  const [personName, setPersonName] = React.useState([]);
+  const handleChange = (event) => {
+    const {
+      target: { value }
+    } = event;
+    setPersonName(
+      // On autofill we get a the stringified value.
+      typeof value === 'string' ? value.split(',') : value
+    );
+  };
+  const style = {
+    position: 'relative',
+    borderRadius: '10px',
+    bgcolor: 'background.paper',
+    boxShadow: 24,
+    p: 4
+  };
+  const formik = useFormik({
+    initialValues: {
+      ShiftName: ''
+    }
+  });
+  const { handleSubmit, getFieldProps } = formik;
 
   return (
     <>
@@ -90,6 +140,72 @@ export default function EmployeeMoreMenu({ dulieu, handleOpenToast }) {
           </ListItemIcon>
           <ListItemText primary="Edit" primaryTypographyProps={{ variant: 'body2' }} />
         </MenuItem>
+        <MenuItem onClick={handleOpen} sx={{ color: 'text.secondary' }}>
+          {' '}
+          <ListItemIcon>
+            <Icon icon={editFill} width={24} height={24} />
+          </ListItemIcon>
+          <ListItemText primary="Specialities" primaryTypographyProps={{ variant: 'body2' }} />
+        </MenuItem>
+        <MenuItem sx={{ color: 'text.secondary' }}>
+          {' '}
+          <ListItemIcon>
+            <Icon icon={editFill} width={24} height={24} />
+          </ListItemIcon>
+          <ListItemText primary="Degrees" primaryTypographyProps={{ variant: 'body2' }} />
+        </MenuItem>
+        <Modal
+          open={open}
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <FormikProvider value={formik}>
+            <Form autoComplete="off" noValidate onSubmit={handleSubmit}>
+              <Box sx={style}>
+                <Stack spacing={1}>
+                  <Typography id="modal-modal-title" variant="h6" component="h2">
+                    Add Specialities
+                  </Typography>
+                  <FormControl sx={{ m: 1, width: 300 }}>
+                    <InputLabel id="demo-multiple-checkbox-label">Specialities</InputLabel>
+                    <Select
+                      labelId="demo-multiple-checkbox-label"
+                      id="demo-multiple-checkbox"
+                      multiple
+                      value={personName}
+                      onChange={handleChange}
+                      input={<OutlinedInput label="Specialities" />}
+                      renderValue={(selected) => selected.join(', ')}
+                      MenuProps={MenuProps}
+                    >
+                      {names.map((name) => (
+                        <MenuItem key={name} value={name}>
+                          <Checkbox checked={personName.indexOf(name) > -1} />
+                          <ListItemText primary={name} />
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </FormControl>
+                  <LoadingButton
+                    loading={loading}
+                    fullWidth
+                    size="large"
+                    type="submit"
+                    variant="contained"
+                  >
+                    Add Specialities
+                  </LoadingButton>
+                </Stack>
+              </Box>
+            </Form>
+          </FormikProvider>
+        </Modal>
       </Menu>
     </>
   );
